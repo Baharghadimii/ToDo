198664c3ab1216f5313c180912191e160da44fe5
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-17 17:08:56 -0800

    add google api

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,16 +1,13 @@
 import { useReducer, useEffect } from 'react';
 import axios from "axios";
-import yelpApi from '../api-keys';
-
+import { googleApi, yelpApi } from '../api-keys';
 
 export default function applicationData() {
 
-  const clientId = 'aGe9u46p0CEZYsKQqDOATw';
-  const apiKey = yelpApi;
-
+  //yelp api for restaurants and cafes
   axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search', {
     headers: {
-      Authorization: `Bearer ${apiKey}`,
+      Authorization: `Bearer ${yelpApi}`,
 
     },
     params: {
@@ -18,5 +15,10 @@ export default function applicationData() {
       term: 'Startbucks',
     }
   }).then(res => console.log(res));
+  //google api for books
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=game+of+thrones&key=${googleApi}`)
+    .then(res => console.log(res))
+  //amazon api for products
+  //rotten tomato api for movies and series
 
 }
