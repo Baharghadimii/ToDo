2374b785f6f7edd072defa1c3ac3df0e4b820fa4
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-17 16:25:48 -0800

    add yelp api

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useApplicationData from '../hooks/useApplicationData'
 
 function App() {
+  useApplicationData();
   return (
     <div className="App">
       <header className="App-header">
