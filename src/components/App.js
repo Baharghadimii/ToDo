3705ebf103038805d22abaf926d5abc2404238f0
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-21 21:23:59 -0800

    add requests for fetching data from database

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import Category from './Category';
+import axios from 'axios';
 
 function App() {
   const [state, setState] = React.useState({
-    showList: true
+    showList: true,
+    movies: [],
+    books: [],
+    products: [],
+    restaurants: []
   });
   const changeDisplay = () => {
     setState({ ...state, showList: false })
@@ -13,6 +18,26 @@ function App() {
   const showList = (items) => {
     setState({ ...state, list: items, showList: true });
   }
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get('http://localhost:3001/api/1/movies', {
+        Headers: new Headers({ 'content-type': 'application/json' })
+      })),
+      Promise.resolve(axios.get('http://localhost:3001/api/1/books', {
+        Headers: new Headers({ 'content-type': 'application/json' })
+      })),
+      Promise.resolve(axios.get('http://localhost:3001/api/1/products', {
+        Headers: new Headers({ 'content-type': 'application/json' })
+      })),
+      Promise.resolve(axios.get('http://localhost:3001/api/1/restaurants', {
+        Headers: new Headers({ 'content-type': 'application/json' })
+      }))
+    ]).then(all => {
+
+    })
+  })
+
   // console.log(state);
   return (
     <div className="App">
