df619131908ed05c0f2d90a54a619e212d27409d
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-22 21:21:30 -0800

    add textfields values to state of login component

--- src/components/login.js
+++ src/components/login.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 export default function Login(props) {
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  });
 
   const submit = () => {
-    localStorage.setItem('token', 'bahar');
+    localStorage.setItem('token', state);
     props.login()
   }
+  console.log(state)
   return (
     <Form onSubmit={submit} style={{ width: '40%', margin: '0 auto', marginTop: '8%', textAlign: 'left' }}>
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control onChange={e => setState({ ...state, email: e.target.value })} type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
     </Form.Text>
       </Form.Group>
-
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control onChange={e => setState({ ...state, password: e.target.value })} type="password" placeholder="Password" />
       </Form.Group>
       <Button variant="primary" type="submit" >
         Submit
