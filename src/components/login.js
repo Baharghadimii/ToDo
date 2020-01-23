ed456f9aead3555524c9900521a3c48144fe2b3f
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-22 22:47:15 -0800

    add axios

--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import { stat } from 'fs';
 export default function Login(props) {
   const [state, setState] = useState({
     email: '',
@@ -11,14 +10,9 @@ export default function Login(props) {
 
   const submit = () => {
     localStorage.setItem('token', JSON.stringify(state));
-    console.log('hello')
-    axios.get('http://localhost:3001/api/auth', {
-      params: {
-        email: state.email,
-        password: stat.password
-      }
-    }).then(res => console.log(res));
-    props.login()
+    axios.post(`http://localhost:3001/api/user/`, { state })
+      .then(res => console.log(res));
+    props.login();
   }
   return (
     <Form onSubmit={submit} style={{ width: '40%', margin: '0 auto', marginTop: '8%', textAlign: 'left' }}>
@@ -35,7 +29,7 @@ export default function Login(props) {
       </Form.Group>
       <Button variant="primary" type="submit" >
         Submit
-  </Button>
+      </Button>
     </Form>
   )
 }
