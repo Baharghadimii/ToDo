21a2d4de86aed556e0a0814ddae99b80835f40a7
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-19 15:53:44 -0800

    add animation to vase layaer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
 
 export default function Home() {
+  const [showVase, setShowVase] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowVase(true);
+    }, 700)
+  }, [])
   return (
     <div data-aos='fade-right'>
       <img style={{ width: '60%', height: '40rem' }} src='https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+1%403x.png'></img>
+      {showVase &&
+        <img data-aos='fade-right' style={{ marginTop: '17rem', marginLeft: '-18rem', width: '15%', height: '20rem' }} src="https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+1%403x+(2).png"></img>
+      }
     </div>
   )
 }
