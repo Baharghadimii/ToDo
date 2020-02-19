510b2023af8c1ef89b29a76a2cbbdcf2ee752ed9
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-19 15:39:07 -0800

    add animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div data-aos='fade-right'>
+      <img style={{ width: '60%', height: '40rem' }} src='https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+1%403x.png'></img>
     </div>
   )
 }
