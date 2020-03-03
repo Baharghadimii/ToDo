138c06afbed44cd730ad407227a45b7a4cb856e5
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-03-02 18:26:27 -0800

    fix the pictures for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,12 +26,24 @@ export default function Home() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row', width: '100%', height: '80rem' }}>
       <div style={{ width: '55%' }}>
-        <img data-aos='fade-right' style={{ width: '100%', height: '40rem' }} src='https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+1%403x.png'></img>
+        <img
+          data-aos='fade-right'
+          style={{ width: '100%', height: '40rem' }}
+          src='https://smarttodo.s3.ca-central-1.amazonaws.com/Asset+1%403x.png'></img>
         {assetOne &&
-          <img data-aos='fade-right' style={{ marginTop: '8rem', marginLeft: '-15rem', width: '25%', height: '20rem' }} src="https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+1%403x+(2).png"></img>
+          <img
+            data-aos='fade-right'
+            style={{ marginTop: '8rem', marginLeft: '-15rem', width: '25%', height: '20rem' }}
+            src="https://smarttodo.s3.ca-central-1.amazonaws.com/Asset+1%403x+(2).png"></img>
         }
-        {assetTwo && <img data-aos='fade-right' style={{ marginLeft: '-41rem', width: '80%', height: '30rem' }} src="https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+1%403x+(3).png" />}
-        {assetThree && <img data-aos='fade-right' style={{ marginTop: '3.5rem', marginLeft: '-45rem', width: '45%', height: '25rem' }} src="https://smarttodolist.s3.ca-central-1.amazonaws.com/Asset+2%403x.png" />}
+        {assetTwo && <img
+          data-aos='fade-right'
+          style={{ marginLeft: '-41rem', width: '80%', height: '30rem' }}
+          src="https://smarttodo.s3.ca-central-1.amazonaws.com/Asset+1%403x+(3).png" />}
+        {assetThree && <img
+          data-aos='fade-right'
+          style={{ marginTop: '3.5rem', marginLeft: '-45rem', width: '45%', height: '25rem' }}
+          src="https://smarttodo.s3.ca-central-1.amazonaws.com/Asset+2%403x.png" />}
 
       </div>
       {mainText &&
