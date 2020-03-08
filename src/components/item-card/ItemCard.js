04fafde3340396ca12cd34d2a7fac6c497891852
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-03-07 21:54:01 -0800

    change star icon color on click

--- src/components/item-card/ItemCard.js
+++ src/components/item-card/ItemCard.js
@@ -7,6 +7,7 @@ export default function ItemCard(props) {
     const token = JSON.parse(localStorage.getItem('token'));
     token.favorites.push(props.item);
     localStorage.setItem('token', JSON.stringify(token));
+    document.getElementById('star').style.color = '#ff2d56';
   }
   return (
     <div
@@ -51,7 +52,7 @@ export default function ItemCard(props) {
         </div>
 
         <FaLink style={{ float: 'right', marginRight: '2rem' }} size={25} color='#3750b2' onClick={() => window.open(props.item.link)} />
-        <FaStar style={{ float: 'right', marginRight: '1rem', cursor: 'pointer' }} size={25} color='#3750b2' onClick={handleFavorite} />
+        <FaStar id="star" style={{ float: 'right', marginRight: '1rem', cursor: 'pointer' }} size={25} color='#3750b2' onClick={handleFavorite} />
       </div>
     </div>
   )
